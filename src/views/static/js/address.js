047ad2e97960fdfd82ce54d8ecab41afc6878a3d
hahnmichaelf <hahn.michael.f@gmail.com>
2018-11-05 20:30:58 -0500

    Shift from local endpoint

--- src/views/static/js/address.js
+++ src/views/static/js/address.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
-    $.getJSON("http://localhost:5112/v1/check/" + $("h1").html(), function(data) {
-        $.getJSON("http://localhost:5112/v1/balance/" + data.coin + "/" + $("h1").html(), function(val) {
+    $.getJSON("https://api.cryptoscamdb.org/v1/check/" + $("h1").html(), function(data) {
+        $.getJSON("https://api.cryptoscamdb.org/v1/balance/" + data.coin + "/" + $("h1").html(), function(val) {
             $("#balance").html(val.balance.toFixed(2) + " " + data.coin.toUpperCase());
             $("#value").html("$"+ val.usdvalue.toFixed(2) + " ($" + (val.usdvalue / val.balance).toFixed(2) + " USD/" + data.coin.toUpperCase() + ")");
             $("#explorer").attr("href", val.blockexplorer);

9f924185f1bc13f83d2a6564762966539638b5e2
Luit Hollander <luit@hollander.email>
2018-11-29 07:45:33 +0100

    Minor fixes for scams page (#32)
    * Move to page 1 when sorting is changed
    
    * Make query parameters case insensitive
    
    * Add "no results" message
    
    * Add loading animation
    
    * Hardcode dependencies
    
    * Update all dependencies and save-exact
    
    * Bump version to 0.5.0
    
    * formatting stuff idk
    
    * Add query parameters when changing state
    
    * Fix pagination active / disabled
    
    * Fixed color issue :)

--- src/views/static/js/scams.js
+++ src/views/static/js/scams.js
@@ -23,22 +23,22 @@ function loadScams(callback) {
 function filterScams() {
 	if(query.coin) {
 		scams = scams.filter(function(scam) {
-			return scam.coin == query.coin;
+			return scam.coin && scam.coin.toUpperCase() == query.coin.toUpperCase();
 		});
 	}
 	if(query.category) {
 		scams = scams.filter(function(scam) {
-			return scam.category == query.category;
+			return scam.category && scam.category.toLowerCase() == query.category.toLowerCase();
 		});
 	}
 	if(query.subcategory) {
 		scams = scams.filter(function(scam) {
-			return scam.subcategory == query.subcategory;
+			return scam.subcategory && scam.subcategory.toLowerCase() == query.subcategory.toLowerCase();
 		});
 	}
 	if(query.status) {
 		scams = scams.filter(function(scam) {
-			return scam.status == query.status;
+			return scam.status && scam.status.toLowerCase() == query.status.toLowerCase();
 		});
 	}
 }
@@ -70,17 +70,18 @@ function sortScams() {
 function changePage(newPage) {
 	page = newPage;
 	if(sorting) {
-		history.replaceState({},"",'/scams/' + page + '/' + sorting);
+		history.replaceState({},"",'/scams/' + page + '/' + sorting + location.search);
 	} else {
-		history.replaceState({},"",'/scams/' + page);
+		history.replaceState({},"",'/scams/' + page + location.search);
 	}
 	renderScams();
 	renderPagination();
 }
 
 function changeSorting(newSorting) {
+	page = 1;
 	sorting = newSorting;
-	history.replaceState({},"",'/scams/' + page + '/' + sorting);
+	history.replaceState({},"",'/scams/1/' + sorting + location.search);
 	sortScams();
 	renderScams();
 	renderPagination();
@@ -89,47 +90,56 @@ function changeSorting(newSorting) {
 function renderScams() {
 	$("tbody").html("");
 	let index = [((page-1) * MAX_RESULTS_PER_PAGE),(page * MAX_RESULTS_PER_PAGE)];
-	for (var i = index[0]; i < index[1]; i++) {
-		const scam = scams[i];
-		const row = $("<tr onclick=\"location.href='/scam/" + scam.id + "'\">");
-		
-		if(scam.name.length > 40) {
-			row.append("<td>" + scam.name.substring(0, 40) + "...</td>");
-		} else {
-			row.append("<td>" + scam.name + "</td>");
+	if(index[1] > scams.length) index[1] = scams.length;
+	if(index[0] in scams) {
+		$(".scams-no-query-results").hide();
+		$(".search-grid-frame").show();
+		$("#results-table").show();
+		$(".next-page").show();
+		for (var i = index[0]; i < index[1]; i++) {
+			const scam = scams[i];
+			const row = $("<tr onclick=\"location.href='/scam/" + scam.id + "'\">");
+			
+			if(scam.name.length > 40) {
+				row.append("<td>" + scam.name.substring(0, 40) + "...</td>");
+			} else {
+				row.append("<td>" + scam.name + "</td>");
+			}
+			
+			if(scam.coin) {
+				row.append("<td><div class='cat-container'><img src='/assets/coins/" + scam.coin + "' alt='Icon - " + scam.coin + "' class='categoryicon cat-icon' style='height:24px; width:24px;' onerror='this.style.display=\"none\"' />  <span class='cat-text'>" + scam.coin + "</span></div></td>");
+			} else {
+				row.append("<td>None</td>");
+			}
+			
+			if(scam.category) {
+				row.append("<td><div class='cat-container'><img src='/assets/" + scam.category.toLowerCase().replace(/\s/g, "") + "' alt='Icon - " + scam.category + "' class='categoryicon cat-icon' style='height:24px; width:24px;' onerror='this.style.display=\"none\"' />  <span class='cat-text'>" + scam.category + "</span></div></td>");
+			} else {
+				row.append("<td>None</td>");
+			}
+			
+			if(scam.subcategory) {
+				row.append("<td><img src='/assets/" + scam.subcategory.toLowerCase().replace(/\s/g, "") + "' alt='Icon - " + scam.subcategory + "' class='subcategoryicon' style='height:24px; width:24px;' onerror='this.style.display=\"none\"' /> " + scam.subcategory + "</td>");
+			} else {
+				row.append("<td>None</td>");
+			}
+			
+			if(scam.status && scam.status == "Active") {
+				row.append("<td><div class='active status-container'><img src='/assets/symbols/exclamation.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Active </span></div></td>");
+			} else if(scam.status && scam.status == "Inactive") {
+				row.append("<td><div class='suspended status-container'><img src='/assets/symbols/check.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Inactive </span></div></td>");
+			} else if(scam.status && scam.status == "Suspended") {
+				row.append("<td><div class='suspended status-container'><img src='/assets/symbols/x.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Suspended </span></div></td>");
+			} else if(scam.status && scam.status == "Offline") {
+				row.append("<td><div class='offline status-container'><img src='/assets/symbols/check.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Offline </span></div></td>");
+			} else {
+				row.append("<td>Unknown</td>");
+			}
+			
+			row.appendTo("tbody");
 		}
-		
-		if(scam.coin) {
-			row.append("<td><div class='cat-container'><img src='/assets/coins/" + scam.coin + "' alt='Icon - " + scam.coin + "' class='categoryicon cat-icon' style='height:24px; width:24px;' onerror='this.style.display=\"none\"' />  <span class='cat-text'>" + scam.coin + "</span></div></td>");
-		} else {
-			row.append("<td>None</td>");
-		}
-		
-		if(scam.category) {
-			row.append("<td><div class='cat-container'><img src='/assets/" + scam.category.toLowerCase().replace(/\s/g, "") + "' alt='Icon - " + scam.category + "' class='categoryicon cat-icon' style='height:24px; width:24px;' onerror='this.style.display=\"none\"' />  <span class='cat-text'>" + scam.category + "</span></div></td>");
-		} else {
-			row.append("<td>None</td>");
-		}
-		
-		if(scam.subcategory) {
-			row.append("<td><img src='/assets/" + scam.subcategory.toLowerCase().replace(/\s/g, "") + "' alt='Icon - " + scam.subcategory + "' class='subcategoryicon' style='height:24px; width:24px;' onerror='this.style.display=\"none\"' /> " + scam.subcategory + "</td>");
-		} else {
-			row.append("<td>None</td>");
-		}
-		
-		if(scam.status && scam.status == "Active") {
-			row.append("<td><div class='active status-container'><img src='/assets/symbols/exclamation.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Active </span></div></td>");
-		} else if(scam.status && scam.status == "Inactive") {
-			row.append("<td><div class='suspended status-container'><img src='/assets/symbols/check.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Inactive </span></div></td>");
-		} else if(scam.status && scam.status == "Suspended") {
-			row.append("<td><div class='suspended status-container'><img src='/assets/symbols/x.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Suspended </span></div></td>");
-		} else if(scam.status && scam.status == "Offline") {
-			row.append("<td><div class='offline status-container'><img src='/assets/symbols/check.svg' alt='Icon - <%= scam.status %>' class='statusicon' style='height:11.5px; width:11.5px;' /> <span class='statustext'> Offline </span></div></td>");
-		} else {
-			row.append("<td>Unknown</td>");
-		}
-		
-		row.appendTo("tbody");
+	} else {
+		$(".scams-no-query-results").show();
 	}
 }
 
@@ -147,13 +157,13 @@ function renderPagination() {
 
     for (var i = arrLoop[0]; i < arrLoop[1]; i++) {
 		const intPageNumber = page+i;
-		let strItemClass = "scams-nav-item";
-		if ((intPageNumber > (scams.length) / MAX_RESULTS_PER_PAGE) || (intPageNumber < 1)) {
-			strItemClass = "disabled scams-nav-item";
+		if ((intPageNumber > Math.ceil(scams.length / MAX_RESULTS_PER_PAGE)) || (intPageNumber < 1)) {
+			strPagination += "<a class='disabled scams-nav-item'>" + intPageNumber + "</a>";
 		} else if (page == intPageNumber) {
-			strItemClass = "active scams-nav-item";
+			strPagination += "<a onclick='changePage(" + intPageNumber + ")' class='btn-active scams-nav-item'>" + intPageNumber + "</a>";
+		} else {
+			strPagination += "<a onclick='changePage(" + intPageNumber + ")' class='scams-nav-item'>" + intPageNumber + "</a>";
 		}
-		strPagination += "<a onclick='changePage(" + intPageNumber + ")' class='" + strItemClass + "'>" + intPageNumber + "</a>";
 	}
 	
 	if (page < Math.ceil(scams.length / MAX_RESULTS_PER_PAGE) - 3) {
@@ -169,8 +179,15 @@ window.addEventListener("load", function() {
 	setInterval(loadStats,20*1000);
 	loadScams(function() {
 		filterScams();
-		sortScams();
-		renderScams();
-		renderPagination();
+		$(".lds-dual-ring").hide();
+		if(scams.length > 0) {
+			sortScams();
+			renderScams();
+			renderPagination();
+		} else if(Object.keys(query).length > 0) {
+			$(".scams-no-query-results").show();
+		} else {
+			$(".scams-zero-entries").show();
+		}
 	});
 });
